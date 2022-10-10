d3f8600ccbe6c1c3134754549eb8431546218e25
Aaron McGuire <aaronmcguiretech@gmail.com>
2022-10-10 15:20:37 -0600

    Removed irrelevant command line param

--- vidswap/change_duration.sh
+++ vidswap/change_duration.sh
@@ -4,12 +4,10 @@ source ./vidswap_core.sh
 
 print_debug=false
 
-while getopts ":n:d" option; do
+while getopts ":d" option; do
     case $option in
         d) #print debugging info
             print_debug=true;;
-        n) #number of video to choose
-            file_to_pick=$OPTARG;;
         \?) #Invalid
             echo "Wrong usage"
             exit 1;;
