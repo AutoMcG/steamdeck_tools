9b3114ab47deed975deb161cc5af613e6eadaf7d
Aaron McGuire <aaronmcguiretech@gmail.com>
2022-11-01 18:50:21 -0600

    Reverted vidswap back to main, removed new version

--- vidswap/vidswap.sh
+++ vidswap/vidswap.sh
@@ -4,46 +4,41 @@ source ./vidswap_core.sh
 
 print_debug=false
 
-while getopts ":d:p:" option; do
+while getopts ":n:d" option; do
     case $option in
         d) #print debugging info
             print_debug=true;;
-        p) #playlist to use
-            playlist_mode=true
-            playlist_input=${OPTARG};;
+        n) #number of video to choose
+            file_to_pick=$OPTARG;;
         \?) #Invalid
             echo "Wrong usage"
             exit 1;;
     esac
 done
 
-echo "playlist_mode: $playlist_mode"
-
-if [[ $playlist_mode=true ]]
+# Maintain backwards compatability/same user experience as established
+# (even though it means calling process_input_files in two places)
+if [[ $file_to_pick -eq 0 ]]
 then
-    should_prompt=false
-else
-    should_prompt=true
-fi
-
-if [ "$should_prompt" = true ]
-then
+    print_scary
+    prompt_continue
     process_input_files
     print_input_files
     prompt_for_vid_pick
 else
-    if [ -f "$playlist_input" ]
-    then
-        read_playlist $playlist_input
-    else
-        create_playlist "" $playlist_input
-    fi
-    pop_playlist_line $playlist_input
+    process_input_files
+    select_vid_file $file_to_pick
 fi
 
 create_override
+#get_sizes
 backup_originals
 print_actions
+#tmp only required for css and js edits with new overrides dir
+#css full screen edit no longer needed
+#copy_to_tmp
+#css_edit
+#truncate_tmp_files
 install_files
 
 if [ "$print_debug" = true ]
